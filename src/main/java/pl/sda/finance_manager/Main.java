61a6d248b6f640a6c2ffa90dd4ef37a58b3021c8
Aleksander Linczuk <aleksanderlinczuk@gmail.com>
2023-04-05 19:41:41 +0200

    feature(FM-1.2): create tables from text resources

--- src/main/java/pl/sda/finance_manager/Main.java
+++ src/main/java/pl/sda/finance_manager/Main.java
@@ -1,5 +1,9 @@
 package pl.sda.finance_manager;
 
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -11,17 +15,20 @@ public class Main {
     public static final String DB_USER = "db_user";
     public static final String DB_PASSWORD = "db_password";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, SQLException {
         try (final Connection connection = DriverManager.getConnection(JDBC_URL + System.getenv(DB_NAME),
-                System.getenv(DB_USER), System.getenv(DB_PASSWORD));){
+                System.getenv(DB_USER), System.getenv(DB_PASSWORD));) {
             System.out.println("Connected to DB!");
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("finance_manager");
+            System.out.println("Hibernate is running as JPA provider " + emf);
+
+            DbInit dbInit = new DbInit(connection);
+            dbInit.initDb();
 
 
-
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
         }
+
 
     }
 }
+]
