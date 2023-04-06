5f671c49db7489f9af193e259a2893c699aac7c2
Aleksander Linczuk <aleksanderlinczuk@gmail.com>
2023-04-06 18:29:51 +0200

    feature(fm): created IncomeRepository- done

--- src/main/java/pl/sda/finance_manager/repository/IncomeRepository.java
+++ src/main/java/pl/sda/finance_manager/repository/IncomeRepository.java
@@ -1,4 +1,68 @@
 package pl.sda.finance_manager.repository;
 
-public class IncomeRepository {
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import pl.sda.finance_manager.DbConnection;
+import pl.sda.finance_manager.entity.Income;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Consumer;
+
+public class IncomeRepository implements Repository<Income, Long> {
+    @Override
+    public void create(Income object) {
+        EntityManager entityManager = DbConnection.getEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(object);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+
+    @Override
+    public Set<Income> findAll() {
+        EntityManager entityManager = DbConnection.getEntityManager();
+        List<Income> incomeList = entityManager.createQuery("SELECT FROM Income", Income.class).getResultList();
+        entityManager.close();
+        return new HashSet<>(incomeList);
+    }
+
+    @Override
+    public Income findById(Long id) {
+        EntityManager entityManager = DbConnection.getEntityManager();
+        Income income = entityManager.find(Income.class, id);
+        entityManager.close();
+        return income;
+    }
+
+    @Override
+    public void update(Income object) {
+        EntityManager entityManager = DbConnection.getEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.merge(object);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+
+    @Override
+    public void delete(Income object) {
+        EntityManager entityManager = DbConnection.getEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.remove(object);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        EntityManager entityManager = DbConnection.getEntityManager();
+        entityManager.getTransaction().begin();
+        Optional<Income> income = Optional.ofNullable(entityManager.find(Income.class, id));
+        income.ifPresent(income1 -> entityManager.remove(income1));
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
 }
